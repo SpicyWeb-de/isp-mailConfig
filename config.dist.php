9038b1beeaf28b1bea42d94c40e5ca3856b21499
dmgeurts <d.m.geurts@geurtscass.com>
2016-11-02 16:44:43 +0000

    Update config.dist.php
    Changed server name to a configurable entity

--- config.dist.php
+++ config.dist.php
@@ -8,4 +8,6 @@ define('SOAP_URI', 'https://SERVER.TLD:8080/remote/');
 // About your service
 define('SERVICE_NAME', 'My Mail Service');
 define('SERVICE_SHORT', 'MyMail');
+define('SERVER_FQDN', echo $this->host['hostname'];
+#define('SERVER_FQDN', 'mail.my-service.com');
 ?>
