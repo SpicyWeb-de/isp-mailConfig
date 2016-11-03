00abe998aa8504da0788d10f5cd944af612a8052
dmgeurts <d.m.geurts@geurtscass.com>
2016-11-03 12:42:19 +0000

    Update config-v1.1.xml.php
    Modified to allow server hostname to be configured in config.php or fetched from the server, the latter gives a single hostname back which pins email to a single server. In my case this is the ISPConfig master server which kills mail redundancy for users. Also added the option to append the email address to the Service name. Useful if users have multiple accounts on the same domain and/or server.

--- include/response/config-v1.1.xml.php
+++ include/response/config-v1.1.xml.php
@@ -3,10 +3,11 @@
 <clientConfig version="1.1">
     <emailProvider id="<?php echo substr($this->user['email'], strpos($this->user['email'], '@')+1) ?>">
         <domain><?php echo substr($this->user['email'], strpos($this->user['email'], '@')+1) ?></domain>
-        <displayName><?php echo SERVICE_NAME ?></displayName>
+        <displayName><?php echo SERVICE_NAME.(SERVICE_ADDR === TRUE ? ' - '.($this->user['login']) : '') ?></displayName>
         <displayShortName><?php echo SERVICE_SHORT ?></displayShortName>
+        // Change order to indicate preference to clients
         <incomingServer type="pop3">
-            <hostname><?php echo SERVER_FQDN ?></hostname>
+            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
             <port>995</port>
             <socketType>SSL</socketType>
             <authentication>password-cleartext</authentication>
@@ -18,14 +19,23 @@
             </pop3>
         </incomingServer>
         <incomingServer type="imap">
-            <hostname><?php echo SERVER_FQDN ?></hostname>
+            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
             <port>993</port>
             <socketType>SSL</socketType>
             <authentication>password-cleartext</authentication>
             <username><?php echo $this->user['login'] ?></username>
         </incomingServer>
+        // Prefer SSL over STARTTLS
         <outgoingServer type="smtp">
-            <hostname><?php echo SERVER_FQDN ?></hostname>
+            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
+            <port>465</port>
+            <socketType>SSL</socketType>
+            <authentication>password-cleartext</authentication>
+            <username><?php echo $this->user['login'] ?></username>
+        </outgoingServer>
+        // Provide STARTTLS as a fallback
+        <outgoingServer type="smtp">
+            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
             <port>587</port>
             <socketType>STARTTLS</socketType>
             <authentication>password-cleartext</authentication>
