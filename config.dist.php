fe2e822fb356717368befc2897e06e19c753578e
dmgeurts <d.m.geurts@geurtscass.com>
2016-11-03 12:33:01 +0000

    Update config.dist.php
    Added option to format Service name as "SERVICE_NAME - $email" instead of just "SERVICE_NAME"

--- config.dist.php
+++ config.dist.php
@@ -7,7 +7,7 @@ define('SOAP_URI', 'https://SERVER.TLD:8080/remote/');
 
 // About your service
 define('SERVICE_NAME', 'My Mail Service');
+define('SERVICE_ADDR', true ); // Add the client email to the Service name? (true/false)
 define('SERVICE_SHORT', 'MyMail');
-define('SERVER_FQDN', echo $this->host['hostname'];
-#define('SERVER_FQDN', 'mail.my-service.com');
+//define('SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
 ?>
