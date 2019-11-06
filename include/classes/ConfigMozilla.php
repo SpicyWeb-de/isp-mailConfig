9d9faf86530d275ea85a70ed7d5a99328548585f
Conrad Sachweh <conrad@csachweh.de>
2019-11-06 21:53:41 +0100

    return static data from request if account not there
    this should prevent account enumeration and is needed, e.g., for Gnome Evolution
    
    fixes #1 and fixes #2

--- include/classes/ConfigMozilla.php
+++ include/classes/ConfigMozilla.php
@@ -1,13 +1,25 @@
 <?php
 class ConfigMozilla extends AutoConfig{
-	
+
 	protected $response_template = 'config-v1.1.xml.php';
 	protected $response_type = 'text/xml';
-	
+
 	public function __construct(){
 		$this->email = urldecode($_GET['emailaddress']);
-		$this->loadData();
+		try {
+		    $this->loadData();
+		} catch (Exception $e) {
+		  /*
+		  this actually works for e.g. Gnome Evolution, they are
+		  more privacy concerned and are sending
+		  `emailaddress=EVOLUTIONUSER%40domain.de&emailmd5=XXX`
+		  just returning the static emailaddress is sufficient
+		  */
+		    $this->host = ["hostname" => "mail.bnbhosting.de"];
+		    $this->user = ["email" => $this->email,
+				   "login" => $this->email];
+		  }
 	}
-	
+
 }
 ?>
--- include/classes/ConfigOutlook.php
+++ include/classes/ConfigOutlook.php
@@ -1,15 +1,21 @@
 <?php
 class ConfigOutlook extends AutoConfig{
-	
+
 	protected $response_template = 'autodiscover.xml.php';
 	protected $response_type = 'text/xml';
-	
+
 	public function __construct(){
         $data = file_get_contents("php://input");
         preg_match("/\<EMailAddress\>(.*?)\<\/EMailAddress\>/", $data, $matches);
         $this->email = $matches[1];
-        $this->loadData();
+	try {
+            $this->loadData();
+	    } catch (Exception $e) {
+		    $this->host = ["hostname" => "mail.bnbhosting.de"];
+		    $this->user = ["login" => $this->email];
+	      }
+
 	}
-	
+
 }
 ?>
