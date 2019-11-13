8c1bb1efbbf5756f4aaf77e51e0168afd886a778
Conrad Sachweh <conrad@csachweh.de>
2019-11-13 21:04:43 +0100

    split server name into smtp, imap and pop

--- config.dist.php
+++ config.dist.php
@@ -9,5 +9,7 @@ define('SOAP_URI', 'https://SERVER.TLD:8080/remote/');
 define('SERVICE_NAME', 'My Mail Service');
 define('SERVICE_ADDR', true ); // Add the client email to the Service name? (true/false)
 define('SERVICE_SHORT', 'MyMail');
-//define('SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
+//define('SMTP_SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
+//define('IMAP_SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
+//define('POP_SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
 ?>
--- include/response/config-v1.1.xml.php
+++ include/response/config-v1.1.xml.php
@@ -7,14 +7,14 @@
         <displayShortName><?php echo SERVICE_SHORT ?></displayShortName>
         // Change order to indicate preference to clients
         <incomingServer type="imap">
-            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
+            <hostname><?php echo (defined('IMAP_SERVER_FQDN') ? IMAP_SERVER_FQDN : $this->host['hostname']) ?></hostname>
             <port>993</port>
             <socketType>SSL</socketType>
             <authentication>password-cleartext</authentication>
             <username><?php echo $this->user['login'] ?></username>
         </incomingServer>
         <incomingServer type="pop3">
-            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
+            <hostname><?php echo (defined('POP_SERVER_FQDN') ? POP_SERVER_FQDN : $this->host['hostname']) ?></hostname>
             <port>995</port>
             <socketType>SSL</socketType>
             <authentication>password-cleartext</authentication>
@@ -27,7 +27,7 @@
         </incomingServer>
         // Prefer SSL over STARTTLS
         <outgoingServer type="smtp">
-            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
+            <hostname><?php echo (defined('SMTP_SERVER_FQDN') ? SMTP_SERVER_FQDN : $this->host['hostname']) ?></hostname>
             <port>465</port>
             <socketType>SSL</socketType>
             <authentication>password-cleartext</authentication>
@@ -35,7 +35,7 @@
         </outgoingServer>
         // Provide STARTTLS as a fallback
         <outgoingServer type="smtp">
-            <hostname><?php echo (defined('SERVER_FQDN') ? SERVER_FQDN : $this->host['hostname']) ?></hostname>
+            <hostname><?php echo (defined('SMTP_SERVER_FQDN') ? SMTP_SERVER_FQDN : $this->host['hostname']) ?></hostname>
             <port>587</port>
             <socketType>STARTTLS</socketType>
             <authentication>password-cleartext</authentication>
