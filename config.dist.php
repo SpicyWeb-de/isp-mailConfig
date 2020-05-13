03f088c9c1c0f1044f9b82e40a8f4b893196b47d
Michael Fürmann <michael@spicyweb.de>
2020-05-13 13:41:09 +0200

    Removes hardcoded server FQDN and introduces definable Fallback FQDN with default to SERVER['SERVER_NAME']

--- config.dist.php
+++ config.dist.php
@@ -9,6 +9,7 @@ define('SOAP_URI', 'https://SERVER.TLD:8080/remote/');
 define('SERVICE_NAME', 'My Mail Service');
 define('SERVICE_ADDR', true ); // Add the client email to the Service name? (true/false)
 define('SERVICE_SHORT', 'MyMail');
+//define('FALLBACK_SERVER_FQDN', 'mail.SERVER.TLD'); // Servername to send back if user is not known, defaults to $_SERVER['SERVER_NAME']
 //define('SMTP_SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
 //define('IMAP_SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
 //define('POP_SERVER_FQDN', 'mail.SERVER.TLD'); // Uncomment to use, autodetects the server name otherwise.
--- include/classes/ConfigOutlook.php
+++ include/classes/ConfigOutlook.php
@@ -1,24 +1,24 @@
 <?php
 class ConfigOutlook extends AutoConfig{
 
-	protected $response_template = 'autodiscover.xml.php';
-	protected $response_type = 'text/xml';
+    protected $response_template = 'autodiscover.xml.php';
+    protected $response_type = 'text/xml';
 
-	public function __construct(){
+    public function __construct(){
         $data = file_get_contents("php://input");
-	$matchCount = preg_match("/\<EMailAddress\>(.*?)\<\/EMailAddress\>/", $data, $matches);
-	try {
-	    if ($matchCount > 0) {
-	       $this->email = $matches[1];
-	    } else {
-	       throw new UnkownUser("No user found in XML");
-	    }
+        $matchCount = preg_match("/\<EMailAddress\>(.*?)\<\/EMailAddress\>/", $data, $matches);
+        try {
+            if ($matchCount > 0) {
+                $this->email = $matches[1];
+            } else {
+                throw new UnkownUser("No user found in XML");
+            }
             $this->loadData();
-	} catch (UnkownUser $e) {
-	    $this->host = ["hostname" => "mail.bnbhosting.de"];
-	    $this->user = ["login" => $this->email];
-	  }
-	}
+        } catch (UnkownUser $e) {
+            $this->host = ["hostname" => defined('FALLBACK_SERVER_FQDN') ? FALLBACK_SERVER_FQDN : $_SERVER['SERVER_NAME']];
+            $this->user = ["login" => $this->email];
+        }
+    }
 
 }
 ?>
